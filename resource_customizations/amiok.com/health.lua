a2dba271d9e5b70ce251090b1884e4a278e60ae8
kim-codefresh <kim.aharfi@codefresh.io>
2022-12-15 01:00:38 +0200

    wip

--- resource_customizations/amiok.com/health.lua
+++ resource_customizations/amiok.com/health.lua
@@ -5,10 +5,10 @@ if obj.status ~= nil then
     if obj.status.healthy == true {
       health_status.status = "Healthy"
       health_status.message = "Application is healthy according to amiok.com condition check"
-    } else {
+    else
       health_status.status = "Degraded"
       health_status.message = "Application is degraded according to amiok.com condition check"
-    }
+    end
   end
 end
 
