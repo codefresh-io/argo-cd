16035d58555acb9f2d52b0351ed1199a6645844b
pashakostohrys <pavel@codefresh.io>
2023-05-30 17:48:57 +0300

    fix: linter for backend code

--- util/kustomize/kustomize_test.go
+++ util/kustomize/kustomize_test.go
@@ -39,9 +39,6 @@ func TestKustomizeBuild(t *testing.T) {
 	nameSuffix := "-nameSuffix"
 	namespace := "custom-namespace"
 	kustomize := NewKustomizeApp(appPath, git.NopCreds{}, "", "")
-	env := &v1alpha1.Env{
-		&v1alpha1.EnvEntry{Name: "ARGOCD_APP_NAME", Value: "argo-cd-tests"},
-	}
 	kustomizeSource := v1alpha1.ApplicationSourceKustomize{
 		NamePrefix: namePrefix,
 		NameSuffix: nameSuffix,
@@ -67,7 +64,7 @@ func TestKustomizeBuild(t *testing.T) {
 			},
 		},
 	}
-	objs, images, err := kustomize.Build(&kustomizeSource, nil, env)
+	objs, images, err := kustomize.Build(&kustomizeSource, nil, nil, "")
 	assert.Nil(t, err)
 	if err != nil {
 		assert.Equal(t, len(objs), 2)
@@ -130,7 +127,7 @@ func TestFailKustomizeBuild(t *testing.T) {
 			},
 		},
 	}
-	_, _, err = kustomize.Build(&kustomizeSource, nil, nil)
+	_, _, err = kustomize.Build(&kustomizeSource, nil, nil, "")
 	assert.EqualError(t, err, "expected integer value for count. Received: garbage")
 }
 
@@ -221,7 +218,7 @@ func TestKustomizeBuildForceCommonLabels(t *testing.T) {
 		appPath, err := testDataDir(t, tc.TestData)
 		assert.Nil(t, err)
 		kustomize := NewKustomizeApp(appPath, git.NopCreds{}, "", "")
-		objs, _, err := kustomize.Build(&tc.KustomizeSource, nil, tc.Env)
+		objs, _, err := kustomize.Build(&tc.KustomizeSource, nil, nil, "")
 		switch tc.ExpectErr {
 		case true:
 			assert.Error(t, err)
@@ -313,7 +310,7 @@ func TestKustomizeBuildForceCommonAnnotations(t *testing.T) {
 		appPath, err := testDataDir(t, tc.TestData)
 		assert.Nil(t, err)
 		kustomize := NewKustomizeApp(appPath, git.NopCreds{}, "", "")
-		objs, _, err := kustomize.Build(&tc.KustomizeSource, nil, tc.Env)
+		objs, _, err := kustomize.Build(&tc.KustomizeSource, nil, nil, "")
 		switch tc.ExpectErr {
 		case true:
 			assert.Error(t, err)
@@ -339,7 +336,7 @@ func TestKustomizeCustomVersion(t *testing.T) {
 	env := &v1alpha1.Env{
 		&v1alpha1.EnvEntry{Name: "ARGOCD_APP_NAME", Value: "argo-cd-tests"},
 	}
-	objs, images, err := kustomize.Build(&kustomizeSource, nil, env)
+	objs, images, err := kustomize.Build(&kustomizeSource, nil, env, "")
 	assert.Nil(t, err)
 	if err != nil {
 		assert.Equal(t, len(objs), 2)
