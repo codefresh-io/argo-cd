4c2d82e5fc0ed0079290cf93d8abc36b09280c16
pashakostohrys <pavel@codefresh.io>
2023-10-03 14:59:29 +0300

    feat: stop setup namespace for kustomize in all the cases

--- util/kustomize/kustomize.go
+++ util/kustomize/kustomize.go
@@ -123,16 +123,7 @@ func (k *kustomize) Build(opts *v1alpha1.ApplicationSourceKustomize, kustomizeOp
 	}
 
 	env = append(env, environ...)
-
-	if namespace != "" {
-		cmd := exec.Command(k.getBinaryPath(), "edit", "set", "namespace", "--", namespace)
-		cmd.Dir = k.path
-		_, err := executil.Run(cmd)
-		if err != nil {
-			return nil, nil, err
-		}
-	}
-
+	
 	if opts != nil {
 		if opts.ForceNamespace && namespace != "" {
 			cmd := exec.Command(k.getBinaryPath(), "edit", "set", "namespace", "--", namespace)
