52284ac57920e05024a8466dc48bb02c2fb05218
Denys Melnyk <denis@codefresh.io>
2023-11-24 11:47:27 +0200

    server cleanup

--- event_reporter/server.go
+++ event_reporter/server.go
@@ -4,35 +4,24 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
-	"github.com/argoproj/argo-cd/v2/common"
+	"net/http"
+	"strings"
+
 	event_reporter "github.com/argoproj/argo-cd/v2/event_reporter/controller"
 	applicationpkg "github.com/argoproj/argo-cd/v2/pkg/apiclient/application"
 	appclientset "github.com/argoproj/argo-cd/v2/pkg/client/clientset/versioned"
 	appinformer "github.com/argoproj/argo-cd/v2/pkg/client/informers/externalversions"
 	applisters "github.com/argoproj/argo-cd/v2/pkg/client/listers/application/v1alpha1"
-	repoapiclient "github.com/argoproj/argo-cd/v2/reposerver/apiclient"
 	servercache "github.com/argoproj/argo-cd/v2/server/cache"
-	"github.com/argoproj/argo-cd/v2/server/rbacpolicy"
 	"github.com/argoproj/argo-cd/v2/server/repository"
-	"github.com/argoproj/argo-cd/v2/ui"
-	"github.com/argoproj/argo-cd/v2/util/assets"
 	"github.com/argoproj/argo-cd/v2/util/db"
 	errorsutil "github.com/argoproj/argo-cd/v2/util/errors"
 	"github.com/argoproj/argo-cd/v2/util/healthz"
-	"github.com/argoproj/argo-cd/v2/util/io"
-	"github.com/argoproj/argo-cd/v2/util/rbac"
-	util_session "github.com/argoproj/argo-cd/v2/util/session"
 	settings_util "github.com/argoproj/argo-cd/v2/util/settings"
-	"github.com/redis/go-redis/v9"
 	log "github.com/sirupsen/logrus"
-	"io/fs"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
-	"net/http"
-	"os"
-	"strings"
-	gosync "sync"
 )
 
 const (
@@ -43,26 +32,17 @@ const (
 type EventReporterServer struct {
 	EventReporterServerOpts
 
-	settings       *settings_util.ArgoCDSettings
-	log            *log.Entry
-	sessionMgr     *util_session.SessionManager
-	settingsMgr    *settings_util.SettingsManager
-	enf            *rbac.Enforcer
-	projInformer   cache.SharedIndexInformer
-	projLister     applisters.AppProjectNamespaceLister
-	policyEnforcer *rbacpolicy.RBACPolicyEnforcer
-	appInformer    cache.SharedIndexInformer
-	appLister      applisters.ApplicationLister
-	db             db.ArgoDB
+	settings     *settings_util.ArgoCDSettings
+	log          *log.Entry
+	settingsMgr  *settings_util.SettingsManager
+	projInformer cache.SharedIndexInformer
+	projLister   applisters.AppProjectNamespaceLister
+	appInformer  cache.SharedIndexInformer
+	appLister    applisters.ApplicationLister
+	db           db.ArgoDB
 
 	// stopCh is the channel which when closed, will shutdown the Argo CD server
-	stopCh           chan struct{}
-	userStateStorage util_session.UserStateStorage
-	indexDataInit    gosync.Once
-	indexData        []byte
-	indexDataErr     error
-	staticAssets     http.FileSystem
-	serviceSet       *EventReporterServerSet
+	stopCh chan struct{}
 }
 
 type EventReporterServerSet struct {
@@ -75,10 +55,8 @@ type EventReporterServerOpts struct {
 	Namespace                string
 	KubeClientset            kubernetes.Interface
 	AppClientset             appclientset.Interface
-	RepoClientset            repoapiclient.Clientset
 	ApplicationServiceClient applicationpkg.ApplicationServiceClient
 	Cache                    *servercache.Cache
-	RedisClient              *redis.Client
 	ApplicationNamespaces    []string
 	BaseHRef                 string
 	RootPath                 string
@@ -152,9 +130,6 @@ func (a *EventReporterServer) checkServeErr(name string, err error) {
 // k8s.io/ go-to-protobuf uses protoc-gen-gogo, which comes from gogo/protobuf (a fork of
 // golang/protobuf).
 func (a *EventReporterServer) Run(ctx context.Context) {
-	a.userStateStorage.Init(ctx)
-	svcSet := newEventReporterServiceSet(a)
-	a.serviceSet = svcSet
 	var httpS = a.newHTTPServer(ctx, a.ListenPort)
 	tlsConfig := tls.Config{}
 	tlsConfig.GetCertificate = func(info *tls.ClientHelloInfo) (*tls.Certificate, error) {
@@ -169,7 +144,7 @@ func (a *EventReporterServer) Run(ctx context.Context) {
 	<-a.stopCh
 }
 
-// NewServer returns a new instance of the Argo CD API server
+// NewServer returns a new instance of the Events Reporter API server
 func NewEventReporterServer(ctx context.Context, opts EventReporterServerOpts) *EventReporterServer {
 	settingsMgr := settings_util.NewSettingsManager(ctx, opts.KubeClientset, opts.Namespace)
 	settings, err := settingsMgr.InitializeSettings(true)
@@ -188,35 +163,17 @@ func NewEventReporterServer(ctx context.Context, opts EventReporterServerOpts) *
 	appInformer := appFactory.Argoproj().V1alpha1().Applications().Informer()
 	appLister := appFactory.Argoproj().V1alpha1().Applications().Lister()
 
-	userStateStorage := util_session.NewUserStateStorage(opts.RedisClient)
-	sessionMgr := util_session.NewSessionManager(settingsMgr, projLister, "", nil, userStateStorage)
-	enf := rbac.NewEnforcer(opts.KubeClientset, opts.Namespace, common.ArgoCDRBACConfigMapName, nil)
-	enf.EnableEnforce(false)
-	err = enf.SetBuiltinPolicy(assets.BuiltinPolicyCSV)
-	errorsutil.CheckError(err)
-	enf.EnableLog(os.Getenv(common.EnvVarRBACDebug) == "1")
-
-	policyEnf := rbacpolicy.NewRBACPolicyEnforcer(enf, projLister)
-	enf.SetClaimsEnforcerFunc(policyEnf.EnforceClaims)
-
-	var staticFS fs.FS = io.NewSubDirFS("dist/app", ui.Embedded)
-
 	dbInstance := db.NewDB(opts.Namespace, settingsMgr, opts.KubeClientset)
 
 	a := &EventReporterServer{
 		EventReporterServerOpts: opts,
 		log:                     log.NewEntry(log.StandardLogger()),
 		settings:                settings,
-		sessionMgr:              sessionMgr,
 		settingsMgr:             settingsMgr,
-		enf:                     enf,
 		projInformer:            projInformer,
 		projLister:              projLister,
 		appInformer:             appInformer,
 		appLister:               appLister,
-		policyEnforcer:          policyEnf,
-		userStateStorage:        userStateStorage,
-		staticAssets:            http.FS(staticFS),
 		db:                      dbInstance,
 	}
 
@@ -226,12 +183,4 @@ func NewEventReporterServer(ctx context.Context, opts EventReporterServerOpts) *
 	}
 
 	return a
-}
-
-func newEventReporterServiceSet(a *EventReporterServer) *EventReporterServerSet {
-	repoService := repository.NewServer(a.RepoClientset, a.db, a.enf, a.Cache, a.appLister, a.projInformer, a.Namespace, a.settingsMgr)
-
-	return &EventReporterServerSet{
-		RepoService: repoService,
-	}
 }
