6be897266aa5a1d1cfe31134d52724acda3a9e2c
Oleksandr Saulyak <oleksandr.saulyak@octopus.com>
2024-08-19 11:03:07 +0300

    chore: remove v1 reporter (#327)
    * event-reporter: added errors parser test from v1 reporter
    
    * server: removed v1 reporter code base
    
    * event-reporter: removed check of codefresh reporter version
    
    * removing v1 reporter leftovers
    
    * re-generated files
    
    * updated changelog
    
    * trigger commit
    
    * fix lint issue
    
    * mod tidy
    
    * lint fix

--- util/settings/settings.go
+++ util/settings/settings.go
@@ -218,14 +218,6 @@ type KustomizeSettings struct {
 	Versions     []KustomizeVersion
 }
 
-// CodefreshReporterVersion includes all cf reporter versions
-type CodefreshReporterVersion string
-
-const (
-	CodefreshV1ReporterVersion CodefreshReporterVersion = "v1"
-	CodefreshV2ReporterVersion CodefreshReporterVersion = "v2"
-)
-
 var (
 	ByClusterURLIndexer     = "byClusterURL"
 	byClusterURLIndexerFunc = func(obj interface{}) ([]string, error) {
@@ -446,8 +438,6 @@ const (
 	settingsWebhookMaxPayloadSizeMB = "webhook.maxPayloadSizeMB"
 	// settingsApplicationInstanceLabelKey is the key to configure injected app instance label key
 	settingsApplicationInstanceLabelKey = "application.instanceLabelKey"
-	// settingsCodefreshReporterVersion is the key to configure injected app instance label key
-	settingsCodefreshReporterVersion = "codefresh.reporterVersion"
 	// settingsResourceTrackingMethodKey is the key to configure tracking method for application resources
 	settingsResourceTrackingMethodKey = "application.resourceTrackingMethod"
 	// resourcesCustomizationsKey is the key to the map of resource overrides
@@ -777,18 +767,6 @@ func (mgr *SettingsManager) GetAppInstanceLabelKey() (string, error) {
 	label := argoCDCM.Data[settingsApplicationInstanceLabelKey]
 	if label == "" {
 		return common.LabelKeyAppInstance, nil
-	}
-	return label, nil
-}
-
-func (mgr *SettingsManager) GetCodefreshReporterVersion() (string, error) {
-	argoCDCM, err := mgr.getConfigMap()
-	if err != nil {
-		return "", err
-	}
-	label := argoCDCM.Data[settingsCodefreshReporterVersion]
-	if label == "" {
-		return string(CodefreshV1ReporterVersion), nil
 	}
 	return label, nil
 }
