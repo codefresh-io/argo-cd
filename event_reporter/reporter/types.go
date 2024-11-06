2f4c9b3cba57b03c40c9fa6cd70f5ee55b587843
oleksandr-codefresh <oleksandr.saulyak@codefresh.io>
2024-11-06 12:14:31 +0200

    event-reporter: added new types for variables grouping

---
+++ event_reporter/reporter/types.go
@@ -0,0 +1,34 @@
+package reporter
+
+import (
+	"github.com/argoproj/argo-cd/v2/event_reporter/utils"
+	"github.com/argoproj/argo-cd/v2/pkg/apiclient/application"
+	appv1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	"github.com/argoproj/argo-cd/v2/reposerver/apiclient"
+)
+
+type ReportedResource struct {
+	rs             *appv1.ResourceStatus
+	rsAsAppInfo    *ReportedResourceAsApp // passed if resource is application
+	actualState    *application.ApplicationResourceResponse
+	desiredState   *apiclient.Manifest
+	manifestGenErr *bool
+}
+
+type ReportedResourceAsApp struct {
+	app                 *appv1.Application
+	revisionsMetadata   *utils.AppSyncRevisionsMetadata
+	applicationVersions *apiclient.ApplicationVersions
+}
+
+type ReportedEntityParentApp struct {
+	app                  *appv1.Application
+	appTree              *appv1.ApplicationTree
+	validatedDestination *appv1.ApplicationDestination
+	revisionsMetadata    *utils.AppSyncRevisionsMetadata
+}
+
+type ArgoTrackingMetadata struct {
+	appInstanceLabelKey *string
+	trackingMethod      *appv1.TrackingMethod
+}
