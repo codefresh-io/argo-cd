46548d4df4dbba0142552284b7fe413ad401f2be
Regina Voloshin <regina.voloshin@codefresh.io>
2024-12-19 12:58:16 +0200

    acr-related log messages fixing (#361)

--- reposerver/repository/repository.go
+++ reposerver/repository/repository.go
@@ -3137,6 +3137,6 @@ func (s *Service) GetChangeRevision(ctx context.Context, request *apiclient.Chan
 		}
 	}
 
-	logCtx.Debugf("changes found for application %s in repo %s from revision %s to revision %s", request.AppName, repo.Repo, previousRevision, revision)
+	logCtx.Debugf("changes not found for application %s in repo %s from revision %s to revision %s", request.AppName, repo.Repo, previousRevision, revision)
 	return &apiclient.ChangeRevisionResponse{}, nil
 }
