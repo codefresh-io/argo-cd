9e3bc9d26ff65872d221c83eacb658b7ec630cf4
Noam Gal <noam.gal@octopus.com>
2025-04-20 09:34:11 +0300

    test: add delay before syncing app to avoid sync window blockage
    Added a 1-second delay before the app sync operation in the
    TestGetVirtualProjectMatch test to prevent errors caused by
    sync window restrictions. This change ensures that the test
    runs smoothly without being blocked by the sync window.

--- test/e2e/project_management_test.go
+++ test/e2e/project_management_test.go
@@ -569,6 +569,8 @@ func TestGetVirtualProjectMatch(t *testing.T) {
 		"--path", guestbookPath, "--project", proj.Name, "--dest-server", v1alpha1.KubernetesInternalAPIServerAddr, "--dest-namespace", fixture.DeploymentNamespace())
 	require.NoError(t, err)
 
+	time.Sleep(1 * time.Second)
+
 	// App trying to sync a resource which is not blacked listed anywhere
 	_, err = fixture.RunCli("app", "sync", fixture.Name(), "--resource", "apps:Deployment:guestbook-ui", "--timeout", strconv.Itoa(10))
 	require.ErrorContains(t, err, "blocked by sync window")
