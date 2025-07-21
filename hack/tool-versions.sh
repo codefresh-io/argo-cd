042b01c0c98b827619257fa2287f3d804bd03165
reggie-k <regina.voloshin@codefresh.io>
2025-07-21 09:58:30 +0300

    upgraded helm to 3.18.4 go-git to 5.16.0
    Signed-off-by: reggie-k <regina.voloshin@codefresh.io>

--- hack/tool-versions.sh
+++ hack/tool-versions.sh
@@ -11,6 +11,6 @@
 # Use ./hack/installers/checksums/add-helm-checksums.sh and
 # add-kustomize-checksums.sh to help download checksums.
 ###############################################################################
-helm3_version=3.17.1
+helm3_version=3.18.4
 kustomize5_version=5.6.0
 protoc_version=29.3
