d87834970712f95038f1e88bcf129bd47304a818
Regina Voloshin <regina.voloshin@codefresh.io>
2025-08-05 10:41:26 +0300

    feat: move pkg/exec in-tree and add fatal timeout upgrade with SIGKILL to ARGO_EXEC_TIMEOUT (#419)
    * chore: move pkg/exec in-tree (#22175) (#22460)
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    
    * fix: add fatal timeout upgrade with SIGKILL to ARGO_EXEC_TIMEOUT (closes #20785, #18478) (#22713)
    
    Signed-off-by: Hazel Sudzilouski <dsudzilouski@olin.edu>
    Signed-off-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>
    Co-authored-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>
    
    ---------
    
    Signed-off-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    Signed-off-by: Hazel Sudzilouski <dsudzilouski@olin.edu>
    Signed-off-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>
    Co-authored-by: Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
    Co-authored-by: Hazel Sudzilouski <t-danielsu@microsoft.com>
    Co-authored-by: Alexandre Gaudreault <alexandre_gaudreault@intuit.com>

--- util/exec/exec_test.go
+++ util/exec/exec_test.go
@@ -203,9 +203,9 @@ func TestRunInDir(t *testing.T) {
 }
 
 func TestRedact(t *testing.T) {
-	assert.Empty(t, Redact(nil)(""))
-	assert.Empty(t, Redact([]string{})(""))
-	assert.Empty(t, Redact([]string{"foo"})(""))
+	assert.Equal(t, "", Redact(nil)(""))
+	assert.Equal(t, "", Redact([]string{})(""))
+	assert.Equal(t, "", Redact([]string{"foo"})(""))
 	assert.Equal(t, "foo", Redact([]string{})("foo"))
 	assert.Equal(t, "******", Redact([]string{"foo"})("foo"))
 	assert.Equal(t, "****** ******", Redact([]string{"foo", "bar"})("foo bar"))
