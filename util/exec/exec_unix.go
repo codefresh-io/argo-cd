54dafb28aa2e7297d8668a933ca01a63bac9f617
reggie-k <regina.voloshin@codefresh.io>
2025-09-15 16:51:19 +0300

    start the process with it's own group and send signals to the entire process group
    Signed-off-by: reggie-k <regina.voloshin@codefresh.io>

--- util/exec/exec_unix.go
+++ util/exec/exec_unix.go
@@ -8,6 +8,7 @@ import "syscall"
 func newSysProcAttr(setpgid bool) *syscall.SysProcAttr {
 	return &syscall.SysProcAttr{Setpgid: setpgid}
 }
+
 func sysCallSignal(pid int, sig syscall.Signal) error {
 	return syscall.Kill(pid, sig)
 }
