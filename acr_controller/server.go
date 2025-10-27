457569610065fc0410d71db39f4c1ad1dc309a05
Patroklos Papapetrou <ppapapetrou76@gmail.com>
2025-10-27 10:03:33 +0200

    address linter issues reported

--- acr_controller/server.go
+++ acr_controller/server.go
@@ -2,14 +2,11 @@ package acr_controller
 
 import (
 	"context"
-	"crypto/tls"
 	"fmt"
 	"net"
 	"net/http"
 	"time"
 
-	applisters "github.com/argoproj/argo-cd/v3/pkg/client/listers/application/v1alpha1"
-	settings_util "github.com/argoproj/argo-cd/v3/util/settings"
 	"github.com/redis/go-redis/v9"
 	log "github.com/sirupsen/logrus"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -36,15 +33,11 @@ var backoff = wait.Backoff{
 type ACRServer struct {
 	ACRServerOpts
 
-	settings             *settings_util.ArgoCDSettings
-	log                  *log.Entry
 	appInformer          cache.SharedIndexInformer
-	appLister            applisters.ApplicationLister
 	applicationClientset appclientset.Interface
 
 	// stopCh is the channel which when closed, will shutdown the Event Reporter server
-	stopCh     chan struct{}
-	serviceSet *ACRServerSet
+	stopCh chan struct{}
 }
 
 type ACRServerSet struct{}
@@ -104,7 +97,7 @@ func (a *ACRServer) Init(ctx context.Context) {
 }
 
 func (a *ACRServer) RunController(ctx context.Context) {
-	controller := acr_controller.NewApplicationChangeRevisionController(a.appInformer, a.Cache, a.ApplicationServiceClient, a.appLister, a.applicationClientset)
+	controller := acr_controller.NewApplicationChangeRevisionController(a.appInformer, a.ApplicationServiceClient, a.applicationClientset)
 	go controller.Run(ctx)
 }
 
@@ -163,10 +156,6 @@ func (a *ACRServer) Listen() (*Listeners, error) {
 // golang/protobuf).
 func (a *ACRServer) Run(ctx context.Context, lns *Listeners) {
 	httpS := a.newHTTPServer(ctx, a.ListenPort)
-	tlsConfig := tls.Config{}
-	tlsConfig.GetCertificate = func(_ *tls.ClientHelloInfo) (*tls.Certificate, error) {
-		return a.settings.Certificate, nil
-	}
 	go func() { a.checkServeErr("httpS", httpS.Serve(lns.Main)) }()
 	go a.RunController(ctx)
 
@@ -187,13 +176,10 @@ func NewApplicationChangeRevisionServer(_ context.Context, opts ACRServerOpts) *
 	appFactory := appinformer.NewSharedInformerFactoryWithOptions(opts.AppClientset, 0, appinformer.WithNamespace(appInformerNs), appinformer.WithTweakListOptions(func(_ *metav1.ListOptions) {}))
 
 	appInformer := appFactory.Argoproj().V1alpha1().Applications().Informer()
-	appLister := appFactory.Argoproj().V1alpha1().Applications().Lister()
 
 	server := &ACRServer{
 		ACRServerOpts:        opts,
-		log:                  log.NewEntry(log.StandardLogger()),
 		appInformer:          appInformer,
-		appLister:            appLister,
 		applicationClientset: opts.AppClientset,
 	}
 
--- cmd/application-change-revision-controller/commands/application_change_revision_controller.go
+++ cmd/application-change-revision-controller/commands/application_change_revision_controller.go
@@ -128,13 +128,9 @@ func NewCommand() *cobra.Command {
 			lns, err := changeRevisionServer.Listen()
 			errors.CheckError(err)
 			for {
-				var closer func()
 				ctx, cancel := context.WithCancel(ctx)
 				changeRevisionServer.Run(ctx, lns)
 				cancel()
-				if closer != nil {
-					closer()
-				}
 			}
 		},
 	}
